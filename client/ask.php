8457528a0f79d4a554dd01302da31e2a7deb4a86
Shihab <mohammadshihabuddin7@gmail.com>
2026-07-07 00:54:19 +0600

    ask qn not impleented just ui added

---
+++ client/ask.php
@@ -0,0 +1,19 @@
+<div class="container">
+<h1 class="heading">Ask A Question </h1>
+
+<form action="./server/requests.php" method="post">
+
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="title" class="form-label">Title</label>
+    <input type="text" name="title" class="form-control" id="title" placeholder="Emter question">
+  </div>
+ 
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+  <button type="submit" name="login" class="btn btn-primary">Submit</button>
+
+  </div>
+
+</form>
+
+</div>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discuss Project</title>
+    <?php include ("./client/commonfiles.php")?>
+</head>
+<body>
+ <?php 
+ session_start();
+ include ("./client/header.php");
+ if (isset($_GET['signup']) && !isset($_SESSION['user']['username'])){
+    include ("./client/signUp.php");
+ }
+ elseif(isset($_GET['login']) && !isset($_SESSION['user']['username'])){
+    include("./client/login.php");
+ }
+ elseif(isset($_GET['ask'])){
+    //
+    include("./client/ask.php");
+ }
+ ?>
+
+</body>
+</html>
